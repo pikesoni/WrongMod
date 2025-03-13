cd1ddf2841d957f117c7cfa4c506f7f43c87318f
pikesoni <Kayakcrasher@gmail.com>
2025-03-12 21:22:44 -0400

    Add Wood Cross

--- src/main/java/pik/wrongmod/WrongMod.java
+++ src/main/java/pik/wrongmod/WrongMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pik.wrongmod.item.ModItems;
 
 public class WrongMod implements ModInitializer {
 
@@ -12,6 +13,7 @@ public class WrongMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
